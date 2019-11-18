6e1d3eace615d54d167076998c5b85f0914bcebc
vinztheprinze <44948197+vinztheprinze@users.noreply.github.com>
2019-11-18 11:21:21 +0100

    Update accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -2,4 +2,5 @@
 			<h2>Accueil</h2>
 			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
 			<p>Jean-Luc Collinet.</p>
+			<p> vincece </p>
 		</section>
